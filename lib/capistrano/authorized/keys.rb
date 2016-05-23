5e312da1d4c1f4c3b838cfd1bce022cad87c7444
Artin Boghosian <artinboghosian@gmail.com>
2016-05-23 13:32:39 -0700

    Initial commit

---
+++ lib/capistrano/authorized/keys.rb
@@ -0,0 +1,9 @@
+require "capistrano/authorized/keys/version"
+
+module Capistrano
+  module Authorized
+    module Keys
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/capistrano/authorized/keys/version.rb
@@ -0,0 +1,7 @@
+module Capistrano
+  module Authorized
+    module Keys
+      VERSION = "0.1.0"
+    end
+  end
+end
